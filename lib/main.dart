e308339c655fabaf90c7194ed2e6bb6cf08c0fcb
webs127 <davidshittu20@gmail.com>
2022-12-31 14:28:54 -0800

    implemented the provider package
    The provider package was used to provide the OnBoardingViewModel

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:recipe/app/routes.dart';
+import 'package:recipe/ui/views/onboarding/onboarding_view_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +11,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: Routes.generateRoute,
-      initialRoute: RouteManager.splash,
-      title: 'Recipe',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<OnBoardingViewModel>(create: (context) => OnBoardingViewModel())
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: Routes.generateRoute,
+        initialRoute: RouteManager.splash,
+        title: 'Recipe',
 
+      ),
     );
   }
 }

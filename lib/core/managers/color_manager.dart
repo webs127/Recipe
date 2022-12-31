f55819d43ec54d87a28af20f8f8b16f7f7931a21
webs127 <davidshittu20@gmail.com>
2022-12-31 13:04:38 -0800

    color manager for the app

---
+++ lib/core/managers/color_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/core/extensions/color_extension.dart';
+
+class ColorManager {
+  static Color splash = HexColor.hexString("#ED4523");
+  static Color black = HexColor.hexString("#111111");
+}

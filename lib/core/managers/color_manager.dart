4cc47e17579ea8c76ac465f473e4286e6f5028a6
webs127 <davidshittu20@gmail.com>
2022-12-31 13:19:22 -0800

    changed class name to RecipeColorManager

--- lib/core/managers/color_manager.dart
+++ lib/core/managers/color_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recipe/core/extensions/color_extension.dart';
 
-class ColorManager {
+class RecipeColorManager {
   static Color splash = HexColor.hexString("#ED4523");
   static Color black = HexColor.hexString("#111111");
 }

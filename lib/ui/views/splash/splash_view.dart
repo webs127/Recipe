ea1fcb39de575371009e1cd8017ba50f7cc9310b
webs127 <davidshittu20@gmail.com>
2022-12-31 13:20:25 -0800

    implemented navigation to onboarding view

--- lib/ui/views/splash/splash_view.dart
+++ lib/ui/views/splash/splash_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/app/routes.dart';
+import 'package:recipe/core/constants/image_constants.dart';
+import 'package:recipe/core/managers/color_manager.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  void nextPage() => Future.delayed(const Duration(seconds: 3), () => Navigator.pushReplacementNamed(context, RouteManager.onboarding));
+  @override
+  void initState() {
+    super.initState();
+    nextPage();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: RecipeColorManager.splash,
+      body: const Center(
+        child: Image(
+          image: AssetImage(
+            RecipeImageConstants.logo
+          ),
+        ),
+      ),
+    );
   }
 }

17b6d0e310b1a699cf529f1a73fab54e06547687
webs127 <davidshittu20@gmail.com>
2022-12-31 16:55:10 -0800

    a manager for handling texts used in the app

---
+++ lib/core/constants/text_constants.dart
@@ -0,0 +1,9 @@
+class RecipeTextConstants {
+  static const String header1 = "BECOME A CHEF";
+  static const String header2 = "NEW RECIPES";
+  static const String header3 = "EAT HEALTHY";
+  static const String subHeader1 = "Learn from our standard chefs, create your own recipes and be your own chef";
+  static const String subHeader2 = "Learn from over 20k+ new recipes created by our chefs with easy to get ingredients";
+  static const String subHeader3 = "We give you the nutrition content of all our available recipes by our chefs";
+
+}

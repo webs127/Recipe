26db5e12991734ac41422abcab7aad7ba79f2b07
webs127 <davidshittu20@gmail.com>
2022-12-31 21:00:23 -0800

    new texts added

--- lib/core/constants/text_constants.dart
+++ lib/core/constants/text_constants.dart
@@ -5,5 +5,12 @@ class RecipeTextConstants {
   static const String subHeader1 = "Learn from our standard chefs, create your own recipes and be your own chef";
   static const String subHeader2 = "Learn from over 20k+ new recipes created by our chefs with easy to get ingredients";
   static const String subHeader3 = "We give you the nutrition content of all our available recipes by our chefs";
+  static const String email = "Sign up with Email";
+  static const String google = "Sign up with Google";
+  static const String apple = "Sign up with Apple";
+  static const String facebook = "Sign up with Facebook";
+  static const String guest = "Use as Guest";
+  static const String alreadyHaveAnAccount = "Already have an Account? ";
+  static const String login = "Login";
 
 }

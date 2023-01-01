024372c424f87811621827f0619d71963dd4f81d
webs127 <davidshittu20@gmail.com>
2022-12-31 20:31:11 -0800

    new color added

--- lib/core/managers/color_manager.dart
+++ lib/core/managers/color_manager.dart
@@ -5,4 +5,6 @@ class RecipeColorManager {
   static Color splash = HexColor.hexString("#ED4523");
   static Color black = HexColor.hexString("#111111");
   static Color grey = HexColor.hexString("#7A7A7A");
+  static Color white = HexColor.hexString("#F1F1F1");
+  static Color blue = HexColor.hexString("#196CD7");
 }

d9784e4bbc52c76829d78594200255cbb31e756f
webs127 <davidshittu20@gmail.com>
2022-12-31 20:34:44 -0800

    new image urls added

--- lib/core/constants/image_constants.dart
+++ lib/core/constants/image_constants.dart
@@ -7,5 +7,11 @@ class RecipeImageConstants {
   static const String onboardingImage_3 = "$path/onboarding_image_3.png";
   static const String onboardingNextButton = "$path/next_button.png";
   static const String getStartedButton = "$path/get_started_button.png";
+  static const String logoHeaderBlack = "$path/recipe_logo_header_black.png";
+  static const String emailLogo = "$path/email.png";
+  static const String googleLogo = "$path/Google.png";
+  static const String appleLogo = "$path/apple.png";
+  static const String facebookLogo = "$path/Facebook logo.png";
+  static const String guestLogo = "$path/user.png";
 
 }

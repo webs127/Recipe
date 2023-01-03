08a14312621d75bb32af554b3741e1c8711182da
webs127 <davidshittu20@gmail.com>
2023-01-03 01:19:55 -0800

    new provider added
    AccountSetUpViewModel added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe/app/routes.dart';
+import 'package:recipe/ui/views/authentication/account/account_setup_view_model.dart';
+import 'package:recipe/ui/views/authentication/signup/signup_view_model.dart';
 import 'package:recipe/ui/views/onboarding/onboarding_view_model.dart';
 
 void main() {
@@ -13,7 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<OnBoardingViewModel>(create: (context) => OnBoardingViewModel())
+        ChangeNotifierProvider<OnBoardingViewModel>(create: (context) => OnBoardingViewModel()),
+        ChangeNotifierProvider<SignUpViewModel>(create: (context) => SignUpViewModel()),
+        ChangeNotifierProvider<AccountSetUpViewModel>(create: (context) => AccountSetUpViewModel()),
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,

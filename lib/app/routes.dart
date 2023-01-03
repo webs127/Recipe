38ea7d35588db5c26ac9fc90141b3c1a871935ea
webs127 <davidshittu20@gmail.com>
2023-01-03 01:18:11 -0800

    new route added
    home view route added

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/ui/views/authentication/account/account_set_up.dart';
 import 'package:recipe/ui/views/authentication/authentication_landing_view.dart';
+import 'package:recipe/ui/views/authentication/forgotpassword/forgot_password_view.dart';
+import 'package:recipe/ui/views/authentication/resetpassword/reset_password_view.dart';
+import 'package:recipe/ui/views/authentication/signin/signin_view.dart';
+import 'package:recipe/ui/views/authentication/signup/signup_view.dart';
+import 'package:recipe/ui/views/authentication/verification/email_verification.dart';
+import 'package:recipe/ui/views/home/home_view.dart';
 import 'package:recipe/ui/views/onboarding/onboarding_view.dart';
 import 'package:recipe/ui/views/splash/splash_view.dart';
 
@@ -7,6 +14,14 @@ class RouteManager {
   static const String splash = "/";
   static const String onboarding = "/onboarding";
   static const String authentication = "/authentication";
+  static const String home = "/home";
+  static const String signUp = "/authentication/signUp";
+  static const String signIn = "/authentication/signIn";
+  static const String forgotPassword = "/authentication/forgotPassword";
+  static const String resetPassword = "/authentication/resetPassword";
+  static const String email = "/authentication/email";
+  static const String accountSetUp = "/authentication/account_setup";
+
 }
 
 class Routes {
@@ -18,6 +33,20 @@ class Routes {
         return MaterialPageRoute(builder: (_) => const OnBoardingView());
       case RouteManager.authentication:
         return MaterialPageRoute(builder: (_) => const AuthenticationLandingView());
+      case RouteManager.signUp:
+        return MaterialPageRoute(builder: (_) => const SignUpView());
+       case RouteManager.signIn:
+         return MaterialPageRoute(builder: (_) => const SignInView());
+      case RouteManager.email:
+        return MaterialPageRoute(builder: (_) => const EmailVerificationView());
+      case RouteManager.forgotPassword:
+         return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+       case RouteManager.resetPassword:
+         return MaterialPageRoute(builder: (_) => const ResetPasswordView());
+      case RouteManager.accountSetUp:
+        return MaterialPageRoute(builder: (_) => const AccountSetUpView());
+      case RouteManager.home:
+        return MaterialPageRoute(builder: (_) => const HomeView());
       default:
         return unDefined();
     }

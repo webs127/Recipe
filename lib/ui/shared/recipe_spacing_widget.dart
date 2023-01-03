12964fea6e2c755ee5a802cb664caa6b7122a264
webs127 <davidshittu20@gmail.com>
2023-01-02 22:53:38 -0800

    made changes to the values

--- lib/ui/shared/recipe_spacing_widget.dart
+++ lib/ui/shared/recipe_spacing_widget.dart
@@ -7,8 +7,8 @@ class RecipeSpacer extends StatelessWidget {
   const RecipeSpacer._({Key? key}) : width = 0, height = 0, super(key: key);
   const RecipeSpacer.height({Key? key}) : width = 0, height = 16, super(key: key);
   const RecipeSpacer.width({Key? key}) : width = 10, height = 0, super(key: key);
-  const RecipeSpacer.largeHeight({Key? key}) : width = 0, height = 56, super(key: key);
-  const RecipeSpacer.mediumHeight({Key? key}) : width = 0, height = 35, super(key: key);
+  const RecipeSpacer.largeHeight({Key? key}) : width = 0, height = 50, super(key: key);
+  const RecipeSpacer.mediumHeight({Key? key}) : width = 0, height = 33, super(key: key);
 
 
   @override

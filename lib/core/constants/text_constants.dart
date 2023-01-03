4905766b146e72391e29928168beee65030e184d
webs127 <davidshittu20@gmail.com>
2023-01-03 01:15:53 -0800

    new text constants added

--- lib/core/constants/text_constants.dart
+++ lib/core/constants/text_constants.dart
@@ -2,15 +2,50 @@ class RecipeTextConstants {
   static const String header1 = "BECOME A CHEF";
   static const String header2 = "NEW RECIPES";
   static const String header3 = "EAT HEALTHY";
-  static const String subHeader1 = "Learn from our standard chefs, create your own recipes and be your own chef";
-  static const String subHeader2 = "Learn from over 20k+ new recipes created by our chefs with easy to get ingredients";
-  static const String subHeader3 = "We give you the nutrition content of all our available recipes by our chefs";
+  static const String subHeader1 =
+      "Learn from our standard chefs, create your own recipes and be your own chef";
+  static const String subHeader2 =
+      "Learn from over 20k+ new recipes created by our chefs with easy to get ingredients";
+  static const String subHeader3 =
+      "We give you the nutrition content of all our available recipes by our chefs";
   static const String email = "Sign up with Email";
   static const String google = "Sign up with Google";
   static const String apple = "Sign up with Apple";
   static const String facebook = "Sign up with Facebook";
   static const String guest = "Use as Guest";
   static const String alreadyHaveAnAccount = "Already have an Account? ";
-  static const String login = "Login";
-
+  static const String login = "Log in";
+  static const String welcomeText = "We would like to meet you";
+  static const String agree = "I agree to the ";
+  static const String terms = "Terms of Services ";
+  static const String and = "and \n ";
+  static const String or = "or";
+  static const String privacy = "Privacy Policy";
+  static const String createAccount = "Create Account";
+  static const String welcomeBack = "Welcome back!";
+  static const String forgotPassword = "Forgot password";
+  static const String remember = "Remember me";
+  static const String verification = "Email verification";
+  static const String noMail = "Didn't receive a mail? ";
+  static const String resend = "Resend";
+  static const String verify = "Verify";
+  static const String submit = "Submit";
+  static const String continueMess = "Continue";
+  static const String reset = "Reset password";
+  static const String resetMessage =
+      "Please enter your new password in the space provided below";
+  static const String accountSetUp = "Account Set Up";
+  static const String allergic = "Are you allergic to any products";
+  static const String avoid =
+      "We will avoid showing you recipes you cannot eat";
+  static const String continueMessage = "Continue";
+  static const String password = "New Password";
+  static const String join = "Join us and cook like a ";
+  static const String professional = "Professional";
+  static const String confirmPassword = "Confirm Password";
+  static const String success = "Verification successful";
+  static const String provide =
+      "Please provide the address linked to this account";
+  static const String message =
+      "We have a sent a 6 - digit code to the email address provided";
 }

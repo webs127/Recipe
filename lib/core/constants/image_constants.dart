6e275afc52417094deca05e149959c918a8a082f
webs127 <davidshittu20@gmail.com>
2023-01-03 01:15:22 -0800

    new images url added

--- lib/core/constants/image_constants.dart
+++ lib/core/constants/image_constants.dart
@@ -13,5 +13,21 @@ class RecipeImageConstants {
   static const String appleLogo = "$path/apple.png";
   static const String facebookLogo = "$path/Facebook logo.png";
   static const String guestLogo = "$path/user.png";
+  static const String eggs = "$path/eggs.png";
+  static const String eggsImage = "$path/eggs_image.png";
+  static const String nuts = "$path/nuts.png";
+  static const String nutsImage = "$path/nuts_image.png";
+  static const String diary = "$path/diary.png";
+  static const String diaryImage = "$path/diary_image.png";
+  static const String gluten = "$path/gluten.png";
+  static const String glutenImage = "$path/gluten_image.png";
+  static const String soy = "$path/soy.png";
+  static const String soyImage = "$path/soy_image.png";
+  static const String nonVegan = "$path/non_vegan.png";
+  static const String nonVeganImage = "$path/non_vegan_image.png";
+  static const String vegan = "$path/vegan.png";
+  static const String veganImage = "$path/vegan_image.png";
+  static const String wheat = "$path/wheat.png";
+  static const String wheatImage = "$path/wheat_image.png";
 
 }
